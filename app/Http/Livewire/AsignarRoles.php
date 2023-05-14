e107fa5223a68317fb251428ece517d0f09eb796
JALI <furio504@hotmail.com>
2023-05-14 14:07:38 -0600

    Se restauro problema de los estilo y se agroco vista y componente asignar roles

---
+++ app/Http/Livewire/AsignarRoles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AsignarRoles extends Component
+{
+    public function render()
+    {
+        return view('livewire.asignar-roles');
+    }
+}

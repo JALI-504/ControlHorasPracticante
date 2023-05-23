2a1321857bc875e41a5891b3ac346954d8040f9d
unknown <furio504@hotmail.com>
2023-05-23 08:43:57 -0600

    VIsta de horas.index fecha inicial y final de practica funcional

--- app/Models/Hora.php
+++ app/Models/Hora.php
@@ -20,7 +20,8 @@ class Hora extends Model
         'hora_total'
     ];
 
-    public function user(){
+    public function user()
+    {
         return $this->hasMany(User::class, 'user_id', 'id');
     }
 
@@ -28,12 +29,13 @@ class Hora extends Model
     public function getTotalSumaHoras()
     {
         $query = DB::table('horas')
-        ->select('user_id', DB::raw("TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(hora_total))), '%H:%i') AS total_suma_horas"))
-        ->groupBy('user_id')
-        ->get();
+            ->select('user_id', DB::raw("TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC(hora_total))), '%H:%i') AS total_suma_horas"))
+            ->groupBy('user_id')
+            ->get();
 
         return $query;
     }
+
 
     public function getPrimeraFechaPorUsuario()
     {
@@ -43,10 +45,9 @@ class Hora extends Model
     }
 
     public function getUltimaFechaPorUsuario()
-{
-    return $this->select('user_id', DB::raw('MAX(fecha) AS ultima_fecha'))
-        ->groupBy('user_id')
-        ->get();
-}
-
+    {
+        return $this->select('user_id', DB::raw('MAX(fecha) AS ultima_fecha'))
+            ->groupBy('user_id')
+            ->get();
+    }
 }
--- resources/views/livewire/horas.blade.php
+++ resources/views/livewire/horas.blade.php
@@ -1,85 +1,90 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
-    <div class="d-flex">
-        <div class=" me-4">
-          <h1>CONTROL DE HORAS:</h1>
-        </div>
-      </div>
+  {{-- Because she competes with no one, no one can compete with her. --}}
+  <div class="d-flex">
+    <div class=" me-4">
+      <h1>CONTROL DE HORAS:</h1>
+    </div>
+  </div>
 
-@php
-    use App\Models\Hora;
-    $totalHoras = Hora::getTotalSumaHoras();
-@endphp
-      <table class="table table-sm align-middle table-hover" style="align-items: center w-50">
-        <thead class=" thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col" style="width: 35%">Nombre</th>
-            <th scope="col">Fecha Inicio</th>
-            <th scope="col">Facha Actual</th>
-            <th scope="col">Total Horas</th>
-            <th scope="col">Asignar Horas</th>
-            <th scope="col">Registro</th>
-            @can('Admin')
-            <th scope="col">Eliminar</th>
-            @endcan
-          </tr>
-        </thead>
-        <tbody style="">
-          @foreach ($users as $user)
-           @if (auth()->user()->hasRole('Admin') || $user->id == auth()->user()->id)
-    
-       @php
-            $horaUsuario = $totalHoras->firstWhere('user_id', $user->id);
-        @endphp
-          <tr>
-            <th scope="row">{{ $loop->index + 1 }}</th>
-            <td>{{ $user->name }}</td>
-           @foreach ($horas as $hora)
-                    <td>{{ $hora->getPrimeraFechaPorUsuario()->first()->primera_fecha }}</td>
-                    <td>{{ $hora->getUltimaFechaPorUsuario()->first()->ultima_fecha }}</td>
-            @endforeach
-             
-            <td>{{ $horaUsuario ? $horaUsuario->total_suma_horas : 'N/A' }}</td>
+  @php
+  use App\Models\Hora;
+  $totalHoras = Hora::getTotalSumaHoras();
+  @endphp
+  <table class="table table-sm align-middle table-hover" style="align-items: center w-50">
+    <thead class=" thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col" style="width: 35%">Nombre</th>
+        <th scope="col">Fecha Inicio</th>
+        <th scope="col">Facha Actual</th>
+        <th scope="col">Total Horas</th>
+        <th scope="col">Asignar Horas</th>
+        <th scope="col">Registro</th>
+        @can('Admin')
+        <th scope="col">Eliminar</th>
+        @endcan
+      </tr>
+    </thead>
+    <tbody style="">
+      @foreach ($users as $user)
+      @if (auth()->user()->hasRole('Admin') || $user->id == auth()->user()->id)
 
-            <td>
-              <a class
-              ="btn btn-outline-primary mt-1 ml-2" style="ali" href="{{route('hora.create')}}"
-              >Asignar</a>
-            </td>
-            <td>
-              <a class
-              ="btn btn-outline-warning mt-1 ml-2" style="ali" href="{{route('hora.registro', ['id' => $user->id])}}"
-              >Registro</a>
-            </td>
-            @can('Admin')
-            <td>
-              
-              <button class="btn btn-outline-danger mt-1 ml-2" style="ali" data-bs-toggle="modal" data-bs-target="#exampleModal">Eliminar</button>
-                
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title text-black" id="exampleModalLabel" style="color: black">Eliminar hora</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body"  style="color: ">
-                    <p>¿Desea eliminar el regitro de horas?</p> 
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-outline-primary" wire:click="delete({{$user->id}})">Eliminar</button>
-                  </div>
+      @php
+      $primeraFecha = $horas->where('user_id', $user->id)->min('fecha');
+      $ultima_fecha = $horas->where('user_id', $user->id)->max('fecha');
+      $horaUsuario = $totalHoras->firstWhere('user_id', $user->id);
+      @endphp
+      <tr>
+        <th scope="row">{{ $loop->index + 1 }}</th>
+        <td>{{ $user->name }}</td>
+        <td>{{ $primeraFecha }}</td>
+        <td>{{ $ultima_fecha }}</td>
+
+        {{-- @foreach ($horas as $hora) --}}
+
+        <td>{{ $horaUsuario ? $horaUsuario->total_suma_horas : 'N/A' }}</td>
+
+        {{-- @endforeach --}}
+
+
+        <td>
+          <a class="btn btn-outline-primary mt-1 ml-2" style="ali" href="{{route('hora.create')}}">Asignar</a>
+        </td>
+        <td>
+          <a class="btn btn-outline-warning mt-1 ml-2" style="ali"
+            href="{{route('hora.registro', ['id' => $user->id])}}">Registro</a>
+        </td>
+        @can('Admin')
+        <td>
+
+          <button class="btn btn-outline-danger mt-1 ml-2" style="ali" data-bs-toggle="modal"
+            data-bs-target="#exampleModal">Eliminar</button>
+
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-black" id="exampleModalLabel" style="color: black">Eliminar hora</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" style="color: ">
+                  <p>¿Desea eliminar el regitro de horas?</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                  <button type="button" class="btn btn-outline-primary"
+                    wire:click="delete({{$user->id}})">Eliminar</button>
                 </div>
               </div>
             </div>
-            </td>
-             @endcan
-          </tr>
-          @endif
-          @endforeach
-        </tbody>
-    </table>
+          </div>
+        </td>
+        @endcan
+      </tr>
+      @endif
+      @endforeach
+    </tbody>
+  </table>
 
 </div>
